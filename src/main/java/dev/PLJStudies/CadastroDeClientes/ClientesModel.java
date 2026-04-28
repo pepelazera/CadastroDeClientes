f4e17c49434d093ce27f91ad5c70dd7051a1163e
Pedro de Castro Lazera Junior <pedro.junior@emap.ma.gov.br>
2026-04-28 15:01:18 -0300

    Criação da entidade Cliente

--- src/main/java/dev/PLJStudies/CadastroDeClientes/ClientesModel.java
+++ src/main/java/dev/PLJStudies/CadastroDeClientes/ClientesModel.java
@@ -1,4 +1,61 @@
 package dev.PLJStudies.CadastroDeClientes;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro") // Nome da tabela
 public class ClientesModel {
+
+
+    @Id
+
+    // Usado para falar como vamos implementar a tatica de gerar o Id automaticamente
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Nesse caso, a ESTRATEGIA (strategy) utilizada foi o IDENTITY (identificar por numeros)
+    Long id;
+    String nome;
+    String cpf;
+    String email;
+
+    // No args constructor
+    public ClientesModel() {
+    }
+
+    // All args constructor
+    public ClientesModel(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
